2bd43ba7a64317c600da0279322629eeb4ee826b
Mary-Alice Wieland <157980517+maryalicewieland@users.noreply.github.com>
2026-04-24 09:24:57 -0400

    pushing legends

--- index.js
+++ index.js
@@ -9,12 +9,13 @@
 //      3) https://observablehq.com/@d3/bar-line-chart
 //      tooltip:
 //      1) https://d3-graph-gallery.com/graph/interactivity_tooltip.html
+//      2) http://www.d3noob.org/2013/01/making-dashed-line-in-d3js.html
+//      legends:
+//      1) https://d3-graph-gallery.com/graph/custom_legend.html
 
-// make tabs
 const tabs = document.querySelectorAll(".tab");
 const contents = document.querySelectorAll(".content");
 
-// tool tip 
 const tooltip = d3.select("body")
     .append("div")
     .style("position", "absolute")
@@ -24,38 +25,14 @@ const tooltip = d3.select("body")
     .style("border-radius", "4px")
     .style("opacity", 0);
 
-// handle logic in selecting tabs by adding event listener so when tab is clicked on the css class active is added or deleted, 
-// showing and hiding content as needed. This should scale up well if we decide to add another tab.
-tabs.forEach(tab => {
-    tab.addEventListener("click", () => {
-        const target = tab.dataset.tab;
-
-        //Remove all active classes
-        tabs.forEach(t => t.classList.remove("active"));
-        contents.forEach(c => c.classList.remove("active"));
-
-        //Activate content accordingly
-        tab.classList.add("active");
-        document.getElementById(target).classList.add("active");
-
-        //Function to change charts
-        drawTotalChart();
-        drawTimeChart();
-        updateChart(target)
-    })
-})
-
-// draw total emissions bar graph
 function drawTotalChart() {
     const svgID = "#total-chart"
     clearChart(svgID);
 
     const svg = d3.select(svgID);
-    // pull height and width from svg in html
     const width = 600;
     const height = 400;
-    // margin helps labels sit right
-    const margin = { top: 20, right: 30, bottom: 40, left: 50 };
+    const margin = { top: 20, right: 30, bottom: 40, left: 80 };
 
     svg.attr("width", width).attr("height", height);
 
@@ -100,82 +77,49 @@ function drawTotalChart() {
         .attr("class", "segment") 
         .on("mouseover", hoverOnBar)
         .on("mouseout", hoverOutBar);
+
+    svg.append("g")
+        .attr("transform", `translate(0,${height - margin.bottom})`)
+        .call(d3.axisBottom(x));
+
+    svg.append("g")
+        .attr("transform", `translate(${margin.left},0)`)
+        .call(d3.axisLeft(y));  
+
+    svg.append("text")
+        .attr("text-anchor", "middle")
+        .attr("x", width / 2)
+        .attr("y", height - margin.bottom + 40)
+        .text("Fiscal Years");
+
+    svg.append("text")
+        .attr("text-anchor", "middle")
+        .attr("transform", "rotate(-90)")
+        .attr("x", -(height / 2))
+        .attr("y", margin.left - 60)
+        .text("MTCO2e");
+
+    const legend = svg.append("g")
+        .attr("transform", `translate(${width - margin.right - 120}, ${margin.top})`);
         
-    svg.append("g")
-        .attr("transform", `translate(0,${height - margin.bottom})`)
-        .call(d3.axisBottom(x));
-
-    svg.append("g")
-        .attr("transform", `translate(${margin.left},0)`)
-        .call(d3.axisLeft(y));
-    
-    });
-}
-
-// draw total emissions bar graph
-function drawTotalChart() {
-    const svgID = "#total-chart"
-    clearChart(svgID);
-
-    const svg = d3.select(svgID);
-    // pull height and width from svg in html
-    const width = 600;
-    const height = 400;
-    // margin helps labels sit right
-    const margin = { top: 20, right: 30, bottom: 40, left: 50 };
-
-    svg.attr("width", width).attr("height", height);
-
-    d3.csv("data/total.csv").then(data => {
-        data.forEach(d => {
-        d["Scope 1"] = +d["Scope 1"];
-        d["Scope 2"] = +d["Scope 2"];
-        d["Scope 3"] = +d["Scope 3"];
-    });
-
-    const keys = ["Scope 1", "Scope 2", "Scope 3"];
-    const stack = d3.stack().keys(keys);
-    const stackedData = stack(data);
-
-    const x = d3.scaleBand()
-        .domain(data.map(d => d["Fiscal Year"]))
-        .range([margin.left, width - margin.right])
-        .padding(0.2);
-    
-    const y = d3.scaleLinear()
-        .domain([0, d3.max(data, d =>
-        d["Scope 1"] + d["Scope 2"] + d["Scope 3"]
-        )])
-        .nice()
-        .range([height - margin.bottom, margin.top]);
-
-    const color = d3.scaleOrdinal()
-        .domain(keys)
-        .range(["#9e9d9d", "#6d6d6d", "#f55353"]);
-    
-    svg.selectAll("g.layer")
-        .data(stackedData)
-        .join("g")
-        .attr("fill", d => color(d.key))
-        .selectAll("rect")
-        .data(d => d)
+        legend.selectAll("rect")
+        .data(keys)
         .join("rect")
-        .attr("x", d => x(d.data["Fiscal Year"]))
-        .attr("y", d => y(d[1]))
-        .attr("height", d => y(d[0]) - y(d[1]))
-        .attr("width", x.bandwidth())
-        .attr("class", "segment") 
-        .on("mouseover", hoverOnBar)
-        .on("mouseout", hoverOutBar);
+        .attr("x", 0)
+        .attr("y", (d, i) => i * 20)
+        .attr("width", 12)
+        .attr("height", 12)
+        .attr("fill", d => color(d));
 
-    svg.append("g")
-        .attr("transform", `translate(0,${height - margin.bottom})`)
-        .call(d3.axisBottom(x));
+        legend.selectAll("text")
+        .data(keys)
+        .join("text")
+        .attr("x", 18)
+        .attr("y", (d, i) => i * 20 + 10)
+        .text(d => d)
+        .attr("font-size", "12px")
+        .attr("alignment-baseline", "middle");
 
-    svg.append("g")
-        .attr("transform", `translate(${margin.left},0)`)
-        .call(d3.axisLeft(y));
-    
     });
 }
 
@@ -185,7 +129,7 @@ function drawTimeChart() {
     const svg = d3.select(svgID);
     const width = 600; 
     const height = 400; 
-    const margin = { top: 20, right: 30, bottom: 40, left: 50 };
+    const margin = { top: 20, right: 30, bottom: 40, left: 80 };
     svg.attr("width", width).attr("height", height);
 
     d3.csv("data/total.csv").then(function(data) {
@@ -205,7 +149,6 @@ function drawTimeChart() {
         .nice()
         .range([height - margin.bottom, margin.top]);
     
-    //hidden bar graph
     barWidth = 25
     svg.selectAll(".hiddenBars")
      .data(data)
@@ -234,15 +177,14 @@ function drawTimeChart() {
         .x(d => x(d["Fiscal Year"]))
         .y(d => y(d["Scope 3"]));
 
-    // Add the line
     svg.append("path")
         .datum(data)
         .attr("fill","none")
         .attr("stroke", "steelblue")
+        .style("stroke-dasharray", ("10, 10"))
         .attr("stroke-width", 2)
         .attr("d", totalLine);
 
-    // Add the line
     svg.append("path")
         .datum(data)
         .attr("fill","none")
@@ -250,7 +192,6 @@ function drawTimeChart() {
         .attr("stroke-width", 2)
         .attr("d", line1);
     
-    // Add the line
     svg.append("path")
         .datum(data)
         .attr("fill","none")
@@ -258,7 +199,6 @@ function drawTimeChart() {
         .attr("stroke-width", 2)
         .attr("d", line2);
     
-    // Add the line
     svg.append("path")
         .datum(data)
         .attr("fill","none")
@@ -272,20 +212,68 @@ function drawTimeChart() {
 
     svg.append("g")
         .attr("transform", `translate(${margin.left},0)`)
-        .call(d3.axisLeft(y));});
+        .call(d3.axisLeft(y));
+    
+    svg.append("text")
+        .attr("text-anchor", "middle")
+        .attr("x", width / 2)
+        .attr("y", height - margin.bottom + 40)
+        .text("Fiscal Years");
+
+    svg.append("text")
+        .attr("text-anchor", "middle")
+        .attr("transform", "rotate(-90)")
+        .attr("x", -(height / 2))
+        .attr("y", margin.left - 60)
+        .text("MTCO2e");
+        
+        const keys = ["Total", "Scope 1", "Scope 2", "Scope 3"];
+
+        const color = d3.scaleOrdinal()
+        .domain(keys)
+        .range(["steelblue", "lightgrey", "grey", "red"]);
+
+        const legend = svg.append("g")
+        .attr("transform", `translate(${width - margin.right - 120}, ${margin.top})`);
+
+        const legendItems = legend.selectAll("g")
+        .data(keys)
+        .join("g")
+        .attr("transform", (d, i) => `translate(0, ${i * 20})`);
+
+        legendItems.append("line")
+            .attr("x1", 0)
+            .attr("x2",20)
+            .attr("y1", 6)
+            .attr("y2", 6)
+            .attr("stroke", d => color(d))
+            .attr("stroke-width", 2)
+            .attr("stroke-dasharray", d => {
+            if (d === "Total") {
+              return "5,5";
+           } else {
+             return "0";
+           }
+        });
+
+        legendItems.append("text")
+            .attr("x", 25)
+            .attr("y", 6)
+            .attr("alignment-baseline", "middle")
+            .attr("font-size", "12px")
+            .text(d => d);
+            });
+
 }
 
-// draw scope 3 chart
 function drawScope3Chart() {
     const svgID = "#scope3-chart"
     clearChart(svgID);
 
     const svg = d3.select(svgID);
-    // pull height and width from svg in html
-    const width = 800;
-    const height = 600;
-    // margin helps labels sit right
-    const margin = { top: 20, right: 30, bottom: 40, left: 50 };
+    const width = 600;
+    const height = 400;
+    const margin = { top: 20, right: 30, bottom: 40, left: 80 };
 
     svg.attr("width", width).attr("height", height);
 
@@ -354,6 +342,18 @@ function drawScope3Chart() {
         .attr("transform", `translate(${margin.left},0)`)
         .call(d3.axisLeft(y));
     
+    svg.append("text")
+        .attr("text-anchor", "middle")
+        .attr("x", width / 2)
+        .attr("y", height - margin.bottom + 40)
+        .text("Fiscal Years");
+
+    svg.append("text")
+        .attr("text-anchor", "middle")
+        .attr("transform", "rotate(-90)")
+        .attr("x", -(height / 2))
+        .attr("y", margin.left - 60)
+        .text("MTCO2e");
     });
 }
 
@@ -362,8 +362,8 @@ function drawPieChart(yearData) {
     d3.select(svgID).selectAll("*").remove();
 
     const width = 400;
-    const height = 450;
-    const radius = Math.min(width, height) / 2;
+    const height = 350;
+    const radius = Math.min(width, height) / 3;
 
     const svg = d3.select(svgID)
         .attr("width", width)
@@ -374,7 +374,7 @@ function drawPieChart(yearData) {
         svg.append("text")
         .attr("y", height / 2 - 15 )
         .attr("text-anchor", "middle")
-        .text(`Emissions Breakdown (tons CO₂e) – Fiscal Year ${yearData["Fiscal Year"]}`);
+        .text(`Emissions By Part (MTCO2e) – Fiscal Year ${yearData["Fiscal Year"]}`);
     
     const keys = ["Faculty Total", "Staff Total", "Air"];
 
@@ -402,17 +402,6 @@ function drawPieChart(yearData) {
     .text(d => yearData[d.data]);
 }
 
-// function to render chart on tab switch accordingly
-function updateChart(tab) {
-    if (tab == "total") {
-        drawTotalChart();
-    }
-    else if (tab == "scope3") {
-        drawScope3Chart();
-    }
-}
-
-// function to avoid charts stacking
 function clearChart(svgId) {
   d3.select(svgId).selectAll("*").remove();
 }
@@ -444,7 +433,6 @@ function hoverOnBar(event, d){
         .style("top", (event.pageY + 10) + "px");
 }
     
-
 function hoverOutBar(event, d){
     d3.selectAll(".segment")
     .attr("opacity", 1) //return to regular 
@@ -453,5 +441,6 @@ function hoverOutBar(event, d){
     tooltip.style("opacity",0); //hide
 }
 
-drawTimeChart();
 drawTotalChart();
+drawTimeChart();
+drawScope3Chart();
